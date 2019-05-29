1be2f10a259d98bc036ae8a99f090d9b08cd94ea
lleroc <llleroc@yahoo.es>
2019-05-29 11:27:38 -0500

    Routing Secciones

--- Vinculacion/src/app/app.component.ts
+++ Vinculacion/src/app/app.component.ts
@@ -21,8 +21,8 @@ export class AppComponent {
       icon: 'list'
     },
     {
-      title: 'Contaminacion Suelo',
-      url: '/suelos',
+      title: 'Categorias de Preguntas',
+      url: '/secciones',
       icon: 'photos'
     }
   ];
