185f4ea38e667b9bb82513f7df79b2fcf47038e8
lleroc <llleroc@yahoo.es>
2019-05-29 11:16:20 -0500

    camnbios

--- Vinculacion/src/app/app-routing.module.ts
+++ Vinculacion/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { SeccionesPageModule } from './secciones/secciones.module';
+
 
 const routes: Routes = [
   {
@@ -15,12 +15,7 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  },
-
-  { path: 'suelos', loadChildren: './contaminacion-del-sueloes/contaminacion-del-sueloes.module#ContaminacionDelSueloesPageModule' },
-  { path: 'secciones', component: SeccionesPageModule }
-
-
+  }
 ];
 
 @NgModule({
