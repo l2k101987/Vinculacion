424b7726d4d1fe2d1c3f73bf146042d458e39efa
Luis jacho <l2k.jacho87@gmail.com>
2019-05-29 10:26:00 -0500

    aire

---
+++ Vinculacion/src/app/contaminacion-del-aires/contaminacion-del-aires.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contaminacion-del-aires',
+  templateUrl: './contaminacion-del-aires.page.html',
+  styleUrls: ['./contaminacion-del-aires.page.scss'],
+})
+export class ContaminacionDelAiresPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
