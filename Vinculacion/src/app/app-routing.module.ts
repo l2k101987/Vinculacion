dcea47e3fef5189365389180f95bbcad8abc65a7
lleroc <llleroc@yahoo.es>
2019-05-29 10:35:15 -0500

    import seccion

--- Vinculacion/src/app/app-routing.module.ts
+++ Vinculacion/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SeccionesPageModule } from './secciones/secciones.module';
 
 const routes: Routes = [
   {
